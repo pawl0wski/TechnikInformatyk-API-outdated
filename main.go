26d44e114865ac9cbc9500bf14516d44d24c7fa7
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-04-16 18:44:13 +0200

    When server starts open database and update cache

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"github.com/Jeboczek/TechnikInformatykBackend/cache"
+	"github.com/Jeboczek/TechnikInformatykBackend/db"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
 func main() {
+	// Initialize database
+	backendDatabase := db.OpenDb("db.sqlite3")
+	// Initialize and update cache
+	cache.GetCacheInstance().UpdateCache(backendDatabase)
 }
